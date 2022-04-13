44ad5a46e1a56da4f54be77aafcc811a5fdbbb95
Gabriel Leite <gabriel.wq@hotmail.com>
2022-04-13 17:19:39 -0300

    uptade

--- KNN/KNNAnalytics.py
+++ KNN/KNNAnalytics.py
@@ -19,429 +19,250 @@ __email__ = "gabriel.wq@alu.ufc.br"
 __maintainer__ = "Gabriel Costa Leite"
 __status__ = "Production"
 
-class SVMAnalytics():
+class KNNAnalytics():
 
-    def __init__(self, X, Y, C=1, gamma=1, test_size=0.3):
+    def __init__(self, X, Y, kn=3, r=2, test_size=0.3):
         self.X = X
         self.Y = Y
-        self.Xnames = []
-        self.Ynames = []
-        self.supportName = []
-        self.C = C
-        self.gamma = gamma
+        self.testsize = test_size
+        self.classes = list(set(self.Y))
+        self.kn = kn
+        self.r = r
         self.test_size = test_size
-        self.clf = None
-        self.X_train = None
-        self.X_test = None
-        self.Y_train = None
-        self.Y_test = None
-        self.pred = None
-        self.matrizConf = None
+        self.supportName = []
         self.matrizSup = []
+        self.matrizConf = self.matrizConfusao()
 
-        self.preprocessing()
+        self.oneXrest = []
 
-        self.tempoTreino = time.time()
-        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.X, self.Y, test_size=self.test_size)
-        self.clf = SVC(C=self.C, gamma=self.gamma)
-        self.clf.fit(self.X_train, self.Y_train)
-        self.pred = self.clf.predict(self.X_test)
-        self.score = self.clf.decision_function(self.X_test)
-        self.tempoTreino = time.time() - self.tempoTreino
+        for c in self.classes:
+            self.oneXrest.append(list(map(lambda x: 1 if x == c else 0 , self.Y)))
 
-        self.matrizConfusao()
-        self.matrizSuporte()
+        self.pred, self.p = self.knn()
 
-    def preprocessing(self):
-        #Transformando string em int:
-        X_set = []
-        for i in self.X.columns:
-            x_set = list(set(self.X[i]))
-            a = 1
-            for j in x_set:
-                self.X[i] = list(map(lambda x: a if x == j else x , self.X[i]))
-                a += 1
-            X_set.append(x_set)
+    def modelo_multiclass(self):
+        X_train, X_test, Y_train, Y_test = train_test_split(self.X, self.Y, self.testsize)
 
-        Y_set = []
-        y_set = list(set(self.Y))
-        a = 1
-        for j in y_set:
-            self.Y = list(map(lambda x: a if x == j else x , self.Y))
-            a += 1
-        Y_set.append(y_set)
+    def modelo_oneXrest(self):
 
-        """ #Normalizando X:
-        for i in self.X.columns:
-            self.X[i] = self.X[i]/max(self.X[i])
- """
-        self.Ynames = Y_set[0]
-        self.Xnames = X_set
 
-    def matrizConfusao(self):
+    def dMinkowski(self, p, q, r):
+        p = np.array(p)
+        q = np.array(q)
+        s = p - q
+        s = s**r
+        return (s.sum())**(1/r)
+
+    def organizador(my_list1, my_list2):
+        for i in range(len(my_list1)):
+            for j in range(i + 1, len(my_list1)):
+                if my_list1[i] > my_list1[j]:
+                    my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
+                    my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
+        return(my_list1, my_list2)
+
+    def knn(self, X_train, Y_train, X_test):
+        pred = []
+        p = []
+        for i in self.X_test:
+            distances = []
+            index = []
+            for j in range(len(self.X_train)):
+                distances.append(self.dMinkowski(i, self.X_train[j], self.r))
+                index.append(j)
+            distances, index = self.organizador(distances, index)
+            distances = np.array(distances[0:self.kn])
+            index = np.array(index[0:self.kn])
+            y_train = np.array(self.Y_train[index])
+            m = ((distances*y_train).sum())/(distances.sum())
+            p.append(m)
+            pred.append(round(m))
+        return pred, p
+
+    def matrizConfusao(self, Y_test, pred):
         #Obtendo a matriz de confusão:
-        y_set = list(set(self.Y))
-        self.matrizConf = np.zeros((len(y_set), len(y_set)))
+        y_set = self.classes
+        matrizConf = np.zeros((len(y_set), len(y_set)))
         for i in range(len(y_set)): #i -> real
             for j in range(len(y_set)): #j -> previsao
                 nReal = 0 #total
                 nPrev = 0 #acertos
-                for k in range(len(self.Y_test)):
+                for k in range(len(Y_test)):
                     if self.Y_test[k] == y_set[i]:
                         nReal += 1
-                        if self.pred[k] == y_set[j]:
+                        if pred[k] == y_set[j]:
                             nPrev += 1
-                self.matrizConf[i,j] = nPrev
+                matrizConf[i,j] = nPrev
+        return matrizConf
 
     def matrizSuporte(self):
+        supportName = []
+        matrizSup = []
+
         #Positive Prediction:
         PP = []
         for i in range(len(self.matrizConf[0])):
             PP.append(self.matrizConf[:,i].sum())
-        self.supportName.append("PP")
+        supportName.append("PP")
         PP = np.array(PP)
-        self.matrizSup.append(PP)
+        matrizSup.append(PP)
 
         #Negative Prediction:
         PN = []
         for i in range(len(self.matrizConf[0])):
             PN.append(self.matrizConf.sum() - self.matrizConf[:,i].sum())
-        self.supportName.append("PN")
+        supportName.append("PN")
         PN = np.array(PN)
-        self.matrizSup.append(PN)
+        matrizSup.append(PN)
 
         #Actual Positive:
         AP = []
         for i in range(len(self.matrizConf[0])):
             AP.append(self.matrizConf[i].sum())
-        self.supportName.append("AP")
+        supportName.append("AP")
         AP = np.array(AP)
-        self.matrizSup.append(AP)
+        matrizSup.append(AP)
 
         #Actual Negative:
         AN = []
         for i in range(len(self.matrizConf[0])):
             AN.append(self.matrizConf.sum() - self.matrizConf[i].sum())
-        self.supportName.append("AN")
+        supportName.append("AN")
         AN = np.array(AN)
-        self.matrizSup.append(AN)
+        matrizSup.append(AN)
 
         #True positive:
         TP = []
         for i in range(len(self.matrizConf[0])):
             TP.append(self.matrizConf[i,i])
-        self.supportName.append("TP")
+        supportName.append("TP")
         TP = np.array(TP)
-        self.matrizSup.append(TP)
+        matrizSup.append(TP)
 
         #False positive:
         FP = []
         for i in range(len(self.matrizConf[0])):
             FP.append(self.matrizConf[:,i].sum() - self.matrizConf[i,i])
-        self.supportName.append("FP")
+        supportName.append("FP")
         FP = np.array(FP)
-        self.matrizSup.append(FP)
+        matrizSup.append(FP)
 
         #False positive:
         FN = []
         for i in range(len(self.matrizConf[0])):
             FN.append(self.matrizConf[i].sum() - self.matrizConf[i,i])
-        self.supportName.append("FN")
+        supportName.append("FN")
         FN = np.array(FN)
-        self.matrizSup.append(FN)
+        matrizSup.append(FN)
 
         #True negative:
         TN = []
         for i in range(len(self.matrizConf[0])):
             TN.append(self.matrizConf.sum() - self.matrizConf[i].sum() - self.matrizConf[:,i].sum() + self.matrizConf[i,i])
-        self.supportName.append("TN")
+        supportName.append("TN")
         TN = np.array(TN)
-        self.matrizSup.append(TN)
+        matrizSup.append(TN)
 
         #Prevalence:
         prevalence = AP/(AP+AN)
-        self.supportName.append("Prevalence")
-        self.matrizSup.append(prevalence)
+        supportName.append("Prevalence")
+        matrizSup.append(prevalence)
 
         #Accuracy:
         accuracy = (TP+TN)/(AP+AN)
-        self.supportName.append("Accuracy")
-        self.matrizSup.append(accuracy)
+        supportName.append("Accuracy")
+        matrizSup.append(accuracy)
 
         #Positive predictive value (PPV):
         PPV = TP/PP
-        self.supportName.append("PPV")
-        self.matrizSup.append(PPV)
+        supportName.append("PPV")
+        matrizSup.append(PPV)
 
         #False discovery rate (FDR):
         FDR = FP/PP
-        self.supportName.append("FDR")
-        self.matrizSup.append(FDR)
+        supportName.append("FDR")
+        matrizSup.append(FDR)
 
         #False omission rate (FOR):
         FOR = FN/PN
-        self.supportName.append("FOR")
-        self.matrizSup.append(FOR)
+        supportName.append("FOR")
+        matrizSup.append(FOR)
 
         #Negative predictive value (NPV):
         NPV = TN/PN
-        self.supportName.append("NPV")
-        self.matrizSup.append(NPV)
+        supportName.append("NPV")
+        matrizSup.append(NPV)
 
         #True positive rate (TPR), recall, sensitivity (SEN):
         TPR = TP/AP
-        self.supportName.append("TPR")
-        self.matrizSup.append(TPR)
+        supportName.append("TPR")
+        matrizSup.append(TPR)
 
         #False positive rate (FPR):
         FPR = FP/(TN + FP)
-        self.supportName.append("FPR")
-        self.matrizSup.append(FPR)
+        supportName.append("FPR")
+        matrizSup.append(FPR)
 
         #Prevalence threshold (PT):
         PT = ((TPR*FPR)**(1/2) - FPR)/(TPR - FPR)
-        self.supportName.append("PT")
-        self.matrizSup.append(PT)
+        supportName.append("PT")
+        matrizSup.append(PT)
 
         #False negative rate (FNR):
         FNR = FN/AP
-        self.supportName.append("FNR")
-        self.matrizSup.append(FNR)
+        supportName.append("FNR")
+        matrizSup.append(FNR)
 
         #True negative rate (TNR):
         TNR = TN/AN
-        self.supportName.append("TNR")
-        self.matrizSup.append(TNR)
+        supportName.append("TNR")
+        matrizSup.append(TNR)
 
         #Positive likelihood ratio (LRp):
         LRp = TPR/FPR
-        self.supportName.append("LRp")
-        self.matrizSup.append(LRp)
+        supportName.append("LRp")
+        matrizSup.append(LRp)
 
         #Negative likelihood ratio (LRn):
         LRn = FNR/TNR
-        self.supportName.append("LRn")
-        self.matrizSup.append(LRn)
+        supportName.append("LRn")
+        matrizSup.append(LRn)
 
         #Markedness (MK):
         MK = PPV/NPV
-        self.supportName.append("MK")
-        self.matrizSup.append(MK)
+        supportName.append("MK")
+        matrizSup.append(MK)
 
         #Diagnostic odds ratio (DOR):
         DOR = LRp/LRn
-        self.supportName.append("DOR")
-        self.matrizSup.append(DOR)
+        supportName.append("DOR")
+        matrizSup.append(DOR)
 
         #Balanced accuracy (BA):
         BA = TPR/TNR
-        self.supportName.append("BA")
-        self.matrizSup.append(BA)
+        supportName.append("BA")
+        matrizSup.append(BA)
 
         #F1 score:
         F1 = (2*TP)/(2*TP+FP+FN)
-        self.supportName.append("F1 score")
-        self.matrizSup.append(F1)
+        supportName.append("F1 score")
+        matrizSup.append(F1)
 
         #Fowlkes–Mallows index (FM):
         FM = (PPV*TPR)**(1/2)
-        self.supportName.append("FM")
-        self.matrizSup.append(FM)
+        supportName.append("FM")
+        matrizSup.append(FM)
 
         #Matthews correlation coefficient (MCC):
         MCC = (TPR*TNR*PPV*NPV)**(1/2) - (FNR*FPR*FOR*FDR)**(1/2)
-        self.supportName.append("MCC")
-        self.matrizSup.append(MCC)
+        supportName.append("MCC")
+        matrizSup.append(MCC)
 
         #Threat score (TS), critical success index (CSI):
         TS = TP/(TP+FN+FP)
-        self.supportName.append("TS")
-        self.matrizSup.append(TS)
+        supportName.append("TS")
+        matrizSup.append(TS)
 
-        self.matrizSup = np.array(self.matrizSup)
+        return np.array(self.matrizSup), supportName
 
-    def getModelo(self):
-        return self.clf
-    
-    def getRes(self):
-        return self.pred
-
-    def getX_test(self):
-        return self.X_test
-
-    def getX_train(self):
-        return self.X_train
-
-    def getY_train(self):
-        return self.Y_train
-
-    def getY_test(self):
-        return self.Y_test
-
-    def getNames(self):
-        return self.Xnames, self.Ynames
-
-    def getConstants(self):
-        return self.C, self.gamma
-
-    def getTestsize(self):
-        return self.test_size
-
-    def getTempotreino(self):
-        return self.tempoTreino
-
-    def getScore(self):
-        return self.score
-
-    def getMatrizConfusao(self):
-        dfMatrizConf = pd.DataFrame(self.matrizConf, columns=self.Ynames)
-        dfMatrizConf.index = self.Ynames
-        return dfMatrizConf
-        
-    def getMatrizSuporte(self):
-        dfMatrizSup = pd.DataFrame(self.matrizSup.T, columns=self.supportName)
-        dfMatrizSup.index = self.Ynames
-        return dfMatrizSup
-
-    def getAccuracy(self):
-        df = self.getMatrizSuporte()
-        return df['Accuracy'].mean()
-
-    def getAllDataClassifiersGraph(self):
-        fig, axs = plt.subplots(2)
-        fig.suptitle('Data Classifiers')
-
-        def organizador(my_list1, my_list2):
-            for i in range(len(my_list1)):
-                for j in range(i + 1, len(my_list1)):
-                    if my_list1[i] > my_list1[j]:
-                        my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
-                        my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
-            return(my_list1, my_list2)
-
-        Y_, X_ = organizador(self.Y, self.X.values)
-        lens = [0]
-        setY_ = list(set(Y_))
-        soma = 0
-        for i in range(len(setY_)):
-            for j in range(len(Y_)):
-                if Y_[j] == setY_[i]:
-                    soma += 1
-            lens.append(soma)
-
-        for i in range(len(setY_)):
-            axs[0].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1])
-            axs[1].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1]+i*100)
-        axs[0].legend(self.Ynames)
-        axs[0].set_title("Real")
-        axs[1].set_title("Distribuido")
-        for ax in axs.flat:
-            ax.set(xlabel='soma de atributos', ylabel='primeiro atributo')
-        for ax in axs.flat:
-            ax.label_outer()
-
-        return fig
-
-    def getTrainDataClassifiersGraph(self):
-        fig, axs = plt.subplots(2)
-        fig.suptitle('Train Data Classifiers')
-
-        def organizador(my_list1, my_list2):
-            for i in range(len(my_list1)):
-                for j in range(i + 1, len(my_list1)):
-                    if my_list1[i] > my_list1[j]:
-                        my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
-                        my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
-            return(my_list1, my_list2)
-
-        Y_, X_ = organizador(self.Y_train, self.X_train.values)
-        lens = [0]
-        setY_ = list(set(Y_))
-        soma = 0
-        for i in range(len(setY_)):
-            for j in range(len(Y_)):
-                if Y_[j] == setY_[i]:
-                    soma += 1
-            lens.append(soma)
-
-        for i in range(len(setY_)):
-            axs[0].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1])
-            axs[1].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1]+i*100)
-        axs[0].legend(self.Ynames)
-        axs[0].set_title("Real")
-        axs[1].set_title("Distribuido")
-        for ax in axs.flat:
-            ax.set(xlabel='soma de atributos', ylabel='primeiro atributo')
-        for ax in axs.flat:
-            ax.label_outer()
-
-        return fig
-
-    def getTestDataClassifiersGraph(self):
-        fig, axs = plt.subplots(2)
-        fig.suptitle('Test Data Classifiers')
-
-        def organizador(my_list1, my_list2):
-            for i in range(len(my_list1)):
-                for j in range(i + 1, len(my_list1)):
-                    if my_list1[i] > my_list1[j]:
-                        my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
-                        my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
-            return(my_list1, my_list2)
-        Y_, X_ = organizador(self.Y_test, self.X_test.values)
-        lens = [0]
-        setY_ = list(set(Y_))
-        soma = 0
-        for i in range(len(setY_)):
-            for j in range(len(Y_)):
-                if Y_[j] == setY_[i]:
-                    soma += 1
-            lens.append(soma)
-
-        for i in range(len(setY_)):
-            axs[0].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1])
-            axs[1].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1]+i*100)
-        axs[0].legend(self.Ynames)
-        axs[0].set_title("Real")
-        axs[1].set_title("Distribuido")
-        for ax in axs.flat:
-            ax.set(xlabel='soma de atributos', ylabel='primeiro atributo')
-        for ax in axs.flat:
-            ax.label_outer()
-
-        return fig
-
-    def getPredDataClassifiersGraph(self):
-        fig, axs = plt.subplots(2)
-        fig.suptitle('Predicted Data Classifiers')
-
-        def organizador(my_list1, my_list2):
-            for i in range(len(my_list1)):
-                for j in range(i + 1, len(my_list1)):
-                    if my_list1[i] > my_list1[j]:
-                        my_list1[i], my_list1[j] = my_list1[j], my_list1[i]
-                        my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
-            return(my_list1, my_list2)
-
-        Y_, X_ = organizador(self.pred, self.X_test.values)
-        lens = [0]
-        setY_ = list(set(Y_))
-        soma = 0
-        for i in range(len(setY_)):
-            for j in range(len(Y_)):
-                if Y_[j] == setY_[i]:
-                    soma += 1
-            lens.append(soma)
-
-        for i in range(len(setY_)):
-            axs[0].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1])
-            axs[1].scatter(X_[lens[i]:lens[i+1]-1, 0], X_.sum(axis=1)[lens[i]:lens[i+1]-1]+i*100)
-        axs[0].legend(self.Ynames)
-        axs[0].set_title("Real")
-        axs[1].set_title("Distribuido")
-        for ax in axs.flat:
-            ax.set(xlabel='soma de atributos', ylabel='primeiro atributo')
-        for ax in axs.flat:
-            ax.label_outer()
-
-        return fig
+    def 
--- KNN/KNNmain.py
+++ KNN/KNNmain.py
@@ -1,8 +1,10 @@
+from pprint import pformat
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+np.seterr(all="ignore")
 
 def dEuclidian(p, q):
     p = np.array(p)
@@ -32,7 +34,264 @@ def organizador(my_list1, my_list2):
                 my_list2[i], my_list2[j] = my_list2[j], my_list2[i]
     return(my_list1, my_list2)
 
-# Importação do banco de dados a ser utilizado nos exemplos
+def knn(X_train, Y_train, X_test, kn=3, r=2):
+    pred = []
+    p = []
+    for i in X_test:
+        distances = []
+        index = []
+        for j in range(len(X_train)):
+            distances.append(dMinkowski(i, X_train[j], r))
+            index.append(j)
+        distances, index = organizador(distances, index)
+        distances = np.array(distances[0:kn])
+        index = np.array(index[0:kn])
+        y_train = np.array(Y_train[index])
+        m = ((distances*y_train).sum())/(distances.sum())
+        p.append(m)
+        pred.append(round(m))
+    return pred, p
+
+def matrizConfusao(pred, Y_test):
+    y_set = list(set(Y_test))
+    matrizConf = np.zeros((len(y_set), len(y_set)))
+    for i in range(len(y_set)): #i -> real
+        for j in range(len(y_set)): #j -> previsao
+            nReal = 0 #total
+            nPrev = 0 #acertos
+            for k in range(len(Y_test)):
+                if Y_test[k] == y_set[i]:
+                    nReal += 1
+                    if pred[k] == y_set[j]:
+                        nPrev += 1
+            matrizConf[i,j] = nPrev
+    return matrizConf
+
+def matrizSuporte(matrizConf):
+    supportName = []
+    matrizSup = []
+
+    #Positive Prediction:
+    PP = []
+    for i in range(len(matrizConf[0])):
+        PP.append(matrizConf[:,i].sum())
+    supportName.append("PP")
+    PP = np.array(PP)
+    matrizSup.append(PP)
+
+    #Negative Prediction:
+    PN = []
+    for i in range(len(matrizConf[0])):
+        PN.append(matrizConf.sum() - matrizConf[:,i].sum())
+    supportName.append("PN")
+    PN = np.array(PN)
+    matrizSup.append(PN)
+
+    #Actual Positive:
+    AP = []
+    for i in range(len(matrizConf[0])):
+        AP.append(matrizConf[i].sum())
+    supportName.append("AP")
+    AP = np.array(AP)
+    matrizSup.append(AP)
+
+    #Actual Negative:
+    AN = []
+    for i in range(len(matrizConf[0])):
+        AN.append(matrizConf.sum() - matrizConf[i].sum())
+    supportName.append("AN")
+    AN = np.array(AN)
+    matrizSup.append(AN)
+
+    #True positive:
+    TP = []
+    for i in range(len(matrizConf[0])):
+        TP.append(matrizConf[i,i])
+    supportName.append("TP")
+    TP = np.array(TP)
+    matrizSup.append(TP)
+
+    #False positive:
+    FP = []
+    for i in range(len(matrizConf[0])):
+        FP.append(matrizConf[:,i].sum() - matrizConf[i,i])
+    supportName.append("FP")
+    FP = np.array(FP)
+    matrizSup.append(FP)
+
+    #False positive:
+    FN = []
+    for i in range(len(matrizConf[0])):
+        FN.append(matrizConf[i].sum() - matrizConf[i,i])
+    supportName.append("FN")
+    FN = np.array(FN)
+    matrizSup.append(FN)
+
+    #True negative:
+    TN = []
+    for i in range(len(matrizConf[0])):
+        TN.append(matrizConf.sum() - matrizConf[i].sum() - matrizConf[:,i].sum() + matrizConf[i,i])
+    supportName.append("TN")
+    TN = np.array(TN)
+    matrizSup.append(TN)
+
+    #Prevalence:
+    prevalence = AP/(AP+AN)
+    supportName.append("Prevalence")
+    matrizSup.append(prevalence)
+
+    #Accuracy:
+    accuracy = (TP+TN)/(AP+AN)
+    supportName.append("Accuracy")
+    matrizSup.append(accuracy)
+
+    #Positive predictive value (PPV):
+    PPV = TP/PP
+    supportName.append("PPV")
+    matrizSup.append(PPV)
+
+    #False discovery rate (FDR):
+    FDR = FP/PP
+    supportName.append("FDR")
+    matrizSup.append(FDR)
+
+    #False omission rate (FOR):
+    FOR = FN/PN
+    supportName.append("FOR")
+    matrizSup.append(FOR)
+
+    #Negative predictive value (NPV):
+    NPV = TN/PN
+    supportName.append("NPV")
+    matrizSup.append(NPV)
+
+    #True positive rate (TPR), recall, sensitivity (SEN):
+    TPR = TP/AP
+    supportName.append("TPR")
+    matrizSup.append(TPR)
+
+    #False positive rate (FPR):
+    FPR = FP/(TN + FP)
+    supportName.append("FPR")
+    matrizSup.append(FPR)
+
+    #Prevalence threshold (PT):
+    PT = ((TPR*FPR)**(1/2) - FPR)/(TPR - FPR)
+    supportName.append("PT")
+    matrizSup.append(PT)
+
+    #False negative rate (FNR):
+    FNR = FN/AP
+    supportName.append("FNR")
+    matrizSup.append(FNR)
+
+    #True negative rate (TNR):
+    TNR = TN/AN
+    supportName.append("TNR")
+    matrizSup.append(TNR)
+
+    #Positive likelihood ratio (LRp):
+    LRp = TPR/FPR
+    supportName.append("LRp")
+    matrizSup.append(LRp)
+
+    #Negative likelihood ratio (LRn):
+    LRn = FNR/TNR
+    supportName.append("LRn")
+    matrizSup.append(LRn)
+
+    #Markedness (MK):
+    MK = PPV/NPV
+    supportName.append("MK")
+    matrizSup.append(MK)
+
+    #Diagnostic odds ratio (DOR):
+    DOR = LRp/LRn
+    supportName.append("DOR")
+    matrizSup.append(DOR)
+
+    #Balanced accuracy (BA):
+    BA = TPR/TNR
+    supportName.append("BA")
+    matrizSup.append(BA)
+
+    #F1 score:
+    F1 = (2*TP)/(2*TP+FP+FN)
+    supportName.append("F1 score")
+    matrizSup.append(F1)
+
+    #Fowlkes–Mallows index (FM):
+    FM = (PPV*TPR)**(1/2)
+    supportName.append("FM")
+    matrizSup.append(FM)
+
+    #Matthews correlation coefficient (MCC):
+    MCC = (TPR*TNR*PPV*NPV)**(1/2) - (FNR*FPR*FOR*FDR)**(1/2)
+    supportName.append("MCC")
+    matrizSup.append(MCC)
+
+    #Threat score (TS), critical success index (CSI):
+    TS = TP/(TP+FN+FP)
+    supportName.append("TS")
+    matrizSup.append(TS)
+
+    return np.array(matrizSup), supportName
+
+def areaTrap(x1, x2, y1, y2):
+    base = abs(x1 - x2)
+    h = (y1+y2)/2
+    return base*h
+
+def ROC(Y_test, p, label):
+    fpr = []
+    tpr = []
+    FP = 0
+    TP = 0
+    A = 0
+    FP_prev = 0
+    TP_prev = 0
+    f_prev = -1
+    L = Y_test
+    f = p
+    N = 0
+    P = 0
+    for ex in L:
+        if ex == 0:
+            N += 1
+        else:
+            P += 1
+    f, L = organizador(f, L)
+    L = L[::-1]
+    f = f[::-1]
+    for j in range(len(L)):
+        if f[j] != f_prev:
+            fpr.append(FP/N)
+            tpr.append(TP/P)
+            A += areaTrap(FP, FP_prev, TP, TP_prev)
+            f_prev = f[j]
+            FP_prev = FP
+            TP_prev = TP
+        if L[j] == 1:
+            TP += 1
+        else:
+            FP += 1
+    fpr.append(FP/N)
+    tpr.append(TP/P)
+    A += areaTrap(N, FP_prev, P, TP_prev)
+    A = A/(P*N)
+
+    fig = plt.figure()
+    plt.plot([0, 1], [0, 1], linestyle='dashed', color="k")
+    plt.plot(fpr, tpr, label=f'ROC Curve (area = {A})', color="tab:orange")
+    plt.legend()
+    plt.grid()
+    plt.ylabel("True Positeve Rate")
+    plt.xlabel("False Positive Rate")
+    plt.title(f'Receiver Operating Characteristic for Class {label}')
+
+    return fig
+
+# Importação do dataset
 
 iris = datasets.load_iris()
 
@@ -42,27 +301,77 @@ X = iris.data
 
 Y = iris.target
 
+# Obtendo as classes existentes para classificação:
+
+classes = list(set(Y))
+
+# Modelos one x rest:
+
+oneXrest = []
+
+for c in classes:
+    oneXrest.append(np.array(list(map(lambda x: 1 if x == c else 0 , Y))))
+
+# Modelo Multiclasse:
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-kn = 3
-pred = []
-p = []
-for i in X_test:
-    distances = []
-    index = []
-    for j in range(len(X_train)):
-        distances.append(dEuclidian(i, X_train[j]))
-        index.append(j)
-    distances, index = organizador(distances, index)
-    distances = np.array(distances[0:kn])
-    index = np.array(index[0:kn])
-    print(distances)
-    print(index)
-    y_train = np.array(Y_train[index])
-    print(y_train)
-    m = ((distances*y_train).sum())/(distances.sum())
-    p.append(m)
-    pred.append(round(m))
-
+pred, p = knn(X_train, Y_train, X_test)
+    
 print(p)
 print(f'Teste: {Y_test}\nPred:  {np.array(pred)}')
+
+# Matriz Confusão:
+
+matrizConf = matrizConfusao(pred, Y_test)
+dfMatrizConf = pd.DataFrame(matrizConf, columns=classes)
+dfMatrizConf.index = classes
+
+print(dfMatrizConf)
+
+# Matriz Suporte:
+
+matrizSup, supportNames = matrizSuporte(matrizConf)
+dfMatrizSup = pd.DataFrame(matrizSup.T, columns=supportNames)
+dfMatrizSup.index = classes
+
+print(dfMatrizSup)
+
+# Modelo One X Rest:
+fig = []
+
+for i in range(len(oneXrest)):
+    print(f'---------------------------------------------------\nClasse de referência: {classes[i]}')
+
+    y = oneXrest[i]
+    oxrclasses = list(set(y))
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+
+    pred, p = knn(X_train, Y_train, X_test)
+    
+    p = np.array(p)
+    print(p)
+    print(f'Teste: {Y_test}\nPred:  {np.array(pred)}')
+
+    # Matriz Confusão:
+
+    matrizConf = matrizConfusao(pred, Y_test)
+    dfMatrizConf = pd.DataFrame(matrizConf, columns=oxrclasses)
+    dfMatrizConf.index = oxrclasses
+
+    print(dfMatrizConf)
+
+    # Matriz Suporte:
+
+    matrizSup, supportNames = matrizSuporte(matrizConf)
+    dfMatrizSup = pd.DataFrame(matrizSup.T, columns=supportNames)
+    dfMatrizSup.index = oxrclasses
+
+    print(dfMatrizSup)
+
+    # Curva ROC:
+
+    fig.append(ROC(Y_test, p, str(i)))
+
+plt.show()
